070f5ec8b9f2bb17b1d57b5a0db92ba3db7a209f
Your name <parthibhank82@gmail.com>
2014-12-21 00:03:08 +0530

    format method using empty {}

---
+++ str_format.py
@@ -0,0 +1,10 @@
+# This is example of format method in python
+name = 	'PARTHIBHAN'
+age = 20 
+
+print ' {0} is learning python at the age of {1} '.format(name,age)
+print ' playing with python at {0} '.format(age)
+
+# {0} represnts the the first argument passed to foramt method
+# The numbers are optional
+print ' My age is {} '.format(age)
