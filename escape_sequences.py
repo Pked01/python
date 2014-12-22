25b7ade32da4bc03746b2ba244176c41c52ac01f
Your name <parthibhank82@gmail.com>
2014-12-22 21:33:02 +0530

    Add escape sequences example

---
+++ escape_sequences.py
@@ -0,0 +1,15 @@
+# using single qoute inside in string
+# use within double quotes
+print "what's cooking ?"
+# another way is to use backslash
+print "what\'s cooking ?"
+# similarly for double quotes
+print '"Consider it done !"'
+print "\"Consider it done !\""
+# using line feed
+print 'Python is a Object Oriented Scripting language\nIt\'s very elegant'
+# backlash at the end of a line indicates the continuation of the string in the next line
+print "Python is a Object Oriented Scripting language. \
+      It\'s very elegant."
+# same using  triple quotes
+print '''Python is a Object Oriented Scripting language\nIt\'s very elegant'''
