377fe99860a33d82d489cb179cd4ac2322de96b9
Your name <parthibhank82@gmail.com>
2014-12-22 22:23:53 +0530

    Add operators example

---
+++ operators.py
@@ -0,0 +1,29 @@
+# Addition operator
+print 3 + 5
+print 'a' + 'b' + 'c' + 'd'
+# subraction operator
+print 556 -2552
+print 10 - 2
+# multiplication operator
+print 2 * 3
+print 'la' * 3
+# division operator
+print 13 / 4
+print 13.0 / 4
+# modulo operator
+print 4 % 2
+print -23.5 % 5
+# power x^
+print 3**3
+# logical and
+x = True
+y = True
+if x and y:
+	print 'both are true'
+# logical not
+x = not x
+print x
+# logical or
+if x or y:
+	print 'one of them is true'
+
