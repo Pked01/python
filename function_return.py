23f8a278eeb57731e97f14434181b11af9a0a1dd
Your name <parthibhank82@gmail.com>
2014-12-24 21:39:41 +0530

    Add function with return example

---
+++ function_return.py
@@ -0,0 +1,12 @@
+# This function returns the maximum of parameters
+def maximum(a,b):
+    if a > b:
+    	return a
+    elif a == b:
+        return 'The numbers are equal'
+    else:
+        return b
+
+print maximum(5,3)
+print maximum(2,3)
+print maximum(1,1)           	
