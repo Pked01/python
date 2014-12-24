5ced7d0bab4cfa5fb4a4d8f44a888e361ae7ad86
Your name <parthibhank82@gmail.com>
2014-12-24 19:15:03 +0530

    Add first example on functions

---
+++ function1.py
@@ -0,0 +1,10 @@
+# This a function that prints Hello World
+def say_hello():
+	# block belonging to the function
+	print 'Hello World'
+# end of the function
+
+# call the functiom
+say_hello()
+say_hello()
+
