af5c1263673274c9fe35be9f069134244f769390
Your name <parthibhank82@gmail.com>
2014-12-25 23:37:04 +0530

    Add function

--- name_attribute.py
+++ name_attribute.py
@@ -1,7 +1,7 @@
 # The name attribute can be used to find if a module 
 # is run by itself or by any other module
-
-if __name__ == '__main__':
-	print 'This program is run by itself'
-else:
-    print 'This program is imported by another module'	
+def module_running():
+    if __name__ == '__main__':
+	        print 'This program is run by itself'
+    else:
+            print 'This program is imported by another module'	
