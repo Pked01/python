3072790ff589bcf0a4e983f4c54abe047e50df67
Your name <parthibhank82@gmail.com>
2014-12-31 23:29:11 +0530

    Add sequence example

---
+++ ds_using_sequences.py
@@ -0,0 +1,18 @@
+shopping_list = ['apple', 'mang', 'carrot', 'banna']
+name = 'NewYear2015'
+
+# indexing or subscription operator
+print 'The item at 0 is', shopping_list[0]
+print 'The item at 2 is', shopping_list[2]
+
+# Slicing of list #
+print 'item 1 to 3 is', shopping_list[1:3]
+print 'item 2 to end is', shopping_list[2:]
+print 'item 1 and 1 from last', shopping_list[1:-1]
+print 'items start to end is', shopping_list[:]
+
+# Slicing on String #
+print 'character 1 to 3 is', name[1:3]
+print 'character 2 to end is', name[2:]
+print 'character 1 and 1 from last', name[1:-1]
+print 'characters start to end is', name[:]
