2f3b417e19bacae6c551d70ce031c5ff8f15776e
Your name <parthibhank82@gmail.com>
2015-01-01 23:45:35 +0530

    Add fix

--- Sets_in_python.py
+++ Sets_in_python.py
@@ -1,12 +1,13 @@
 # example of set in python
 bri = set(['brasil', 'russia', 'india'])
-print 'india' in bri
-print 'us' in bri
+print 'bri :', bri
+print "'india' in bri :", 'india' in bri
+print "'us' in bri :", 'us' in bri
 bric = bri.copy()
-print 'bric', bric
+print 'bri to bric :', bric
 bric.add('china')
-print 'bric', bric
-print 'bric is superset of bri', bric.issuperset(bri)
+print 'add china to bric :', bric
+print 'bric is superset of bri :', bric.issuperset(bri)
 bric.remove('russia')
-print 'bric', bric
-print 'bri & bric or bri.intersection(bric)', bri & bric
+print 'remove russia from bric', bric
+print 'bri & bric or bri.intersection(bric) condition returns :', bri & bric
