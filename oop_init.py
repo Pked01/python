8a83377229ae6409f7d907ae805e89903ef5f84e
Your name <parthibhank82@gmail.com>
2015-01-06 23:12:02 +0530

    Add comments

--- oop_init.py
+++ oop_init.py
@@ -1,9 +1,11 @@
+# init methods are used to do initialization 
+# while creating a new instance
 class Person:
 	def __init__(self, name):
-		self.name = name
+		self.name = name   # name is a local variable
 
 	def say_hi(self):
-	    print 'Hi, my name is', self.name
+	    print 'Hi, my name is', self.name  # self.name is the field of an Object
 
 p = Person('Parthibhan')
 p.say_hi()	    	
