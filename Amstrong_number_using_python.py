70e1eeede2239f6b03c969d11ca727e5d92ca84f
Your name <parthibhank82@gmail.com>
2015-01-07 23:44:03 +0530

    Add Amstrong number example

---
+++ Amstrong_number_using_python.py
@@ -0,0 +1,14 @@
+number = int(input('enter a number :'))
+copy_of_number = number
+sum = 0
+
+while copy_of_number > 0:
+	last_digit = copy_of_number % 10
+	sum += last_digit ** 3
+	copy_of_number /= 10
+
+if sum == number:
+	print 'The number {} is a Amstrong number'.format(number)
+else:
+    print 'The number {} is not a Amstrong number'.format(number)
+    	
