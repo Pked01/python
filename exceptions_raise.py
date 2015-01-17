b678df11645cc06c7cd307445098182b3a6855be
Your name <parthibhank82@gmail.com>
2015-01-17 23:36:13 +0530

    Add raise exception example

---
+++ exceptions_raise.py
@@ -0,0 +1,18 @@
+class ShortInputException(Exception):
+	'''User-defined exception class'''
+	def __init__(self, length, atleast):
+		Exception.__init__(self)
+		self.length = length
+		self.atleast = atleast
+
+try:
+    text = raw_input('Enter something --> ')
+    if len(text) < 3:
+       raise ShortInputException(len(text), 3)	
+except EOFError:
+    print 'Why did you do EOF on me ?'
+except ShortInputException as ex:
+    print ('ShortInputException: The input was ' + \
+           '{0} Long, excepted atleast {1}').format(ex.length, ex.atleast)
+else:
+    print 'No exception was raised'          
