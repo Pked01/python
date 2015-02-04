876147925fcaace02057d40bfe6334e4f4f10f08
Your name <parthibhank82@gmail.com>
2015-02-04 23:21:47 +0530

    Add example class

---
+++ example_class.py
@@ -0,0 +1,11 @@
+class Student:
+    
+    def __init__(self, name, age, gender):
+
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+s1 = Student('Parthibhan', 21, 'M')        
+print s1
+print s1.age
